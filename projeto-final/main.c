6b722ac4db452ab4e0743f23f4b672fa228bfc00
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2023-11-27 08:37:07 -0300

    Base do structure (feito em aula)

--- projeto-final/main.c
+++ projeto-final/main.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
+struct Carro {
+    char placa[9];
+    char modelo[51];
+    char marca[51];
+    int ano;
+};
+void ler(struct Carro *p_carro);
+void imprimir(struct Carro c);
 int main() {
-    FILE * fp = NULL;
-    int ch;
-    printf("Abrindo arquivo chamado frutas.txt \n");
-    fp = fopen("projeto-final/frutas.txt", "r");
-    if(fp == NULL){
-        printf("Caminho errado ou arquivo inexistente, confira\n");
-        exit(1);
-    }
-    while((ch=fgetc(fp))!=EOF){
-        putchar(ch);
-    }
-    printf("\nDados disponiveis no momento foram impresso acima\n");
-    fclose(fp);
+    struct Carro carro;
+    ler(&carro);
+    imprimir(carro);
     return 0;
+}
+void imprimir(struct Carro c){
+    printf("Apresentado os dados do carro: \n");
+    printf("Modelo: %s \n", c.modelo);
+    printf("Placa: %s \n", c.placa);
+    printf("Marca: %s \n", c.marca);
+    printf("Ano Fabricacao: %d \n", c.ano);
+}
+void ler(struct Carro *p_carro){
+    fflush(stdin);
+    printf("Digite a placa do veiculo: \n");
+    gets(p_carro->placa);
+    fflush(stdin);
+    printf("Digite o modelo do veiculo: \n");
+    gets(p_carro->modelo);
+    printf("Digite a marca do veiculo: \n");
+    fflush(stdin);
+    gets(p_carro->marca);
+    printf("Digite o ano do veiculo: \n");
+    scanf("%d", &p_carro->ano);
 }
