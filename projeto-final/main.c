1b35442fc6bf3d16293d0a9831d392065787c4e9
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2023-11-27 08:10:09 -0300

    teste inicial

--- projeto-final/main.c
+++ projeto-final/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 int main() {
+    typedef struct {
+        int codigo;
+        char titulo[100];
+        char autor[50];
+        int emprestado; // 0 para disponível, 1 para emprestado
+    } Livro;
 
-    
     return 0;
+}
+void cadastrarLivro(Livro* livros, int* totalLivros){
+
 }
